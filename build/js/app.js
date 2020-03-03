f7aa31af4e9187318607993cb27dbd0bae02a083
Bohdan <biriukov.bogdan@yandex.ua>
2020-03-04 01:03:09 +0300

    added slide navigation, and slide cards

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,4 @@
 $(function () {
+
 }); 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJhcHAuanMiXSwic291cmNlc0NvbnRlbnQiOlsiJChmdW5jdGlvbiAoKSB7XHJcbn0pOyAiXSwiZmlsZSI6ImFwcC5qcyJ9
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJhcHAuanMiXSwic291cmNlc0NvbnRlbnQiOlsiJChmdW5jdGlvbiAoKSB7XHJcblxyXG59KTsgIl0sImZpbGUiOiJhcHAuanMifQ==
